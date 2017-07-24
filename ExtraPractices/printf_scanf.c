1ec45d0c576fabf74e503459fb6b2959362f897a
Kuan-yin Chen <cgi0911@gmail.com>
2017-07-24 16:53:41 -0400

    new file:   ExtraPractices/printf_scanf.c

---
+++ ExtraPractices/printf_scanf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main (int argc, char *argv[]) {
+    int a, b;
+    printf ("Input two integers: ");
+    scanf ("%d %d", &a, &b);
+
+    printf ("Your inputs: %d, %d\n", a, b);
+
+    return 0;
+}
